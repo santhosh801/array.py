d0ab2ccbde300d18a136013069e6104e9a832edb
santhosh <146916164+santhosh801@users.noreply.github.com>
2024-01-10 20:45:13 +0530

    Add files via upload

---
+++ day5/array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+      
+        
+        num1= []
+        m=
+        num2=[]
+        n=
+     if m < n :
+         print(num1 "" num2)
+         else :
+             print("none")
+
+  
